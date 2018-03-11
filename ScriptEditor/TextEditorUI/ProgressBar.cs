ea5807eb342276d9b22ef6d3983f93e808468ebb
Mr.Stalin <fakels@yandex.ru>
2018-03-12 02:21:48 +0300

    Fixed the position of progress bar for the message editor.

--- ScriptEditor/TextEditorUI/ProgressBar.cs
+++ ScriptEditor/TextEditorUI/ProgressBar.cs
@@ -12,21 +12,24 @@ namespace ScriptEditor.TextEditorUI
 
         public ProgressBarForm(Form owner, int max)
         {
-            bar = new ProgressBar() { Width = 305, Height = 15, Top = 10, Maximum = max};
-            
+            bar = new ProgressBar() { Width = 305, Height = 15, Top = 14, Maximum = max};
+            var lb = new Label() { Text = "Loading message file...", Top = 0, Left = 10, AutoSize = true};
+
             progressForm = new Form()
             {
-                MinimumSize = new Size(200, 10), Width = 312, Height = 15, 
+                MinimumSize = new Size(200, 20), Width = 312, Height = 20, 
                 ControlBox = false, ShowIcon = false, ShowInTaskbar = false,
-                StartPosition = FormStartPosition.Manual,
+                StartPosition = (owner.Location.IsEmpty) ? FormStartPosition.CenterScreen : FormStartPosition.Manual,
                 FormBorderStyle = FormBorderStyle.FixedSingle
             };
-
+            progressForm.Controls.Add(lb);
             progressForm.Controls.Add(bar);
 
-            progressForm.Location = new Point(owner.Location.X + (owner.Width - progressForm.Width) / 2,
-                                              owner.Location.Y + (owner.Height - progressForm.Height) / 2);
+            if (!owner.Location.IsEmpty)
+                progressForm.Location = new Point(owner.Location.X + (owner.Width - progressForm.Width) / 2,
+                                                  owner.Location.Y + (owner.Height - progressForm.Height) / 2);
             progressForm.Show();
+            Application.DoEvents();
         }
 
         public int SetProgress
